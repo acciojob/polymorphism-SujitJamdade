f5ba9fa75a2590c47cbf9270cf2b604582fd0d90
SujitJamdade <jamdadesujit58@gmail.com>
2023-07-21 18:48:21 +0530

    called function with parameter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public static int product(int x, int y) {
+
+        }
+
+        public static int product(int x, int y, int z) {
+
+        }
+
+        public double product(double x, double y) {
+
+        }
+    }
+
+
+
+    // main method
+    public static void main(String args[]){
+
+        Product p = new Product();
+        p.product(5,2);
+        p.product(5,2,3);
+        p.product(5.0,2.0);
+
+    }
+
 }
